373039ae606f6c8de99ddcb3b2923da49d4cd392
Lawrence Gunn <lawrence@hookflash.com>
2013-03-24 15:00:54 -0700

    Added flies for Blackberry video output device display.

--- hookflash-libs/webRTC/src/modules/video_render/main/source/blackberry/video_render_bb_impl.h
+++ hookflash-libs/webRTC/src/modules/video_render/main/source/blackberry/video_render_bb_impl.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2011 The WebRTC project authors. All Rights Reserved.
+ *  Copyright (c) 2012 The WebRTC project authors. All Rights Reserved.
  *
  *  Use of this source code is governed by a BSD-style license
  *  that can be found in the LICENSE file in the root of the source
@@ -8,126 +8,144 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-#ifndef WEBRTC_MODULES_VIDEO_RENDER_MAIN_SOURCE_EXTERNAL_VIDEO_RENDER_EXTERNAL_IMPL_H_
-#define WEBRTC_MODULES_VIDEO_RENDER_MAIN_SOURCE_EXTERNAL_VIDEO_RENDER_EXTERNAL_IMPL_H_
+#ifndef WEBRTC_MODULES_VIDEO_RENDER_MAIN_SOURCE_BB_VIDEO_RENDER_BB_IMPL_H_
+#define WEBRTC_MODULES_VIDEO_RENDER_MAIN_SOURCE_BB_VIDEO_RENDER_BB_IMPL_H_
 
 #include "i_video_render.h"
-#include "critical_section_wrapper.h"
-#include "module_common_types.h"
+
 
 namespace webrtc {
 
-// Class definitions
-class VideoRenderExternalImpl: IVideoRender, public VideoRenderCallback
-{
-public:
-    /*
-     *   Constructor/destructor
-     */
+class CriticalSectionWrapper;
+class EventWrapper;
+class ThreadWrapper;
 
-    VideoRenderExternalImpl(const WebRtc_Word32 id,
-                            const VideoRenderType videoRenderType,
-                            void* window, const bool fullscreen);
+// The object a module user uses to send new frames to the Blackberry OpenGL ES window
 
-    virtual ~VideoRenderExternalImpl();
+class BlackberryRenderCallback : public VideoRenderCallback {
+ public:
 
-    virtual WebRtc_Word32 Init();
+  virtual WebRtc_Word32 RenderFrame(const WebRtc_UWord32 streamId,
+                                    VideoFrame& videoFrame) = 0;
 
-    virtual WebRtc_Word32 ChangeUniqueId(const WebRtc_Word32 id);
+  virtual ~BlackberryRenderCallback() {};
+};
 
-    virtual WebRtc_Word32 ChangeWindow(void* window);
+class VideoRenderBlackBerry: IVideoRender {
+ public:
+  static WebRtc_Word32 SetAndroidEnvVariables(void* javaVM);
 
-    /**************************************************************************
-     *
-     *   Incoming Streams
-     *
-     ***************************************************************************/
+  VideoRenderAndroid(const WebRtc_Word32 id,
+                     const VideoRenderType videoRenderType,
+                     void* window,
+                     const bool fullscreen);
 
-    virtual VideoRenderCallback
-            * AddIncomingRenderStream(const WebRtc_UWord32 streamId,
-                                      const WebRtc_UWord32 zOrder,
-                                      const float left, const float top,
-                                      const float right, const float bottom);
+  virtual ~VideoRenderAndroid();
 
-    virtual WebRtc_Word32
-            DeleteIncomingRenderStream(const WebRtc_UWord32 streamId);
+  virtual WebRtc_Word32 Init()=0;
 
-    virtual WebRtc_Word32
-            GetIncomingRenderStreamProperties(const WebRtc_UWord32 streamId,
-                                              WebRtc_UWord32& zOrder,
-                                              float& left, float& top,
-                                              float& right, float& bottom) const;
+  virtual WebRtc_Word32 ChangeUniqueId(const WebRtc_Word32 id);
 
-    /**************************************************************************
-     *
-     *   Start/Stop
-     *
-     ***************************************************************************/
+  virtual WebRtc_Word32 ChangeWindow(void* window);
 
-    virtual WebRtc_Word32 StartRender();
+  virtual VideoRenderCallback* AddIncomingRenderStream(
+      const WebRtc_UWord32 streamId,
+      const WebRtc_UWord32 zOrder,
+      const float left, const float top,
+      const float right, const float bottom);
 
-    virtual WebRtc_Word32 StopRender();
+  virtual WebRtc_Word32 DeleteIncomingRenderStream(
+      const WebRtc_UWord32 streamId);
 
-    /**************************************************************************
-     *
-     *   Properties
-     *
-     ***************************************************************************/
+  virtual WebRtc_Word32 GetIncomingRenderStreamProperties(
+      const WebRtc_UWord32 streamId,
+      WebRtc_UWord32& zOrder,
+      float& left, float& top,
+      float& right, float& bottom) const;
 
-    virtual VideoRenderType RenderType();
+  virtual WebRtc_Word32 StartRender();
 
-    virtual RawVideoType PerferedVideoType();
+  virtual WebRtc_Word32 StopRender();
 
-    virtual bool FullScreen();
+  virtual void ReDraw();
 
-    virtual WebRtc_Word32
-            GetGraphicsMemory(WebRtc_UWord64& totalGraphicsMemory,
-                              WebRtc_UWord64& availableGraphicsMemory) const;
+  // Properties
 
-    virtual WebRtc_Word32
-            GetScreenResolution(WebRtc_UWord32& screenWidth,
-                                WebRtc_UWord32& screenHeight) const;
+  virtual VideoRenderType RenderType();
 
-    virtual WebRtc_UWord32 RenderFrameRate(const WebRtc_UWord32 streamId);
+  virtual RawVideoType PerferedVideoType();
 
-    virtual WebRtc_Word32 SetStreamCropping(const WebRtc_UWord32 streamId,
-                                            const float left, const float top,
-                                            const float right,
-                                            const float bottom);
+  virtual bool FullScreen();
 
-    virtual WebRtc_Word32 ConfigureRenderer(const WebRtc_UWord32 streamId,
-                                            const unsigned int zOrder,
-                                            const float left, const float top,
-                                            const float right,
-                                            const float bottom);
+  virtual WebRtc_Word32 GetGraphicsMemory(
+      WebRtc_UWord64& totalGraphicsMemory,
+      WebRtc_UWord64& availableGraphicsMemory) const;
 
-    virtual WebRtc_Word32 SetTransparentBackground(const bool enable);
+  virtual WebRtc_Word32 GetScreenResolution(
+      WebRtc_UWord32& screenWidth,
+      WebRtc_UWord32& screenHeight) const;
 
-    virtual WebRtc_Word32 SetText(const WebRtc_UWord8 textId,
-                                  const WebRtc_UWord8* text,
-                                  const WebRtc_Word32 textLength,
-                                  const WebRtc_UWord32 textColorRef,
-                                  const WebRtc_UWord32 backgroundColorRef,
-                                  const float left, const float top,
-                                  const float right, const float bottom);
+  virtual WebRtc_UWord32 RenderFrameRate(const WebRtc_UWord32 streamId);
 
-    virtual WebRtc_Word32 SetBitmap(const void* bitMap,
-                                    const WebRtc_UWord8 pictureId,
-                                    const void* colorKey, const float left,
-                                    const float top, const float right,
-                                    const float bottom);
+  virtual WebRtc_Word32 SetStreamCropping(const WebRtc_UWord32 streamId,
+                                          const float left, const float top,
+                                          const float right,
+                                          const float bottom);
 
-    // VideoRenderCallback
-    virtual WebRtc_Word32 RenderFrame(const WebRtc_UWord32 streamId,
-                                      VideoFrame& videoFrame);
+  virtual WebRtc_Word32 SetTransparentBackground(const bool enable);
 
-private:
-    WebRtc_Word32 _id;
-    CriticalSectionWrapper& _critSect;
-    bool _fullscreen;
+  virtual WebRtc_Word32 ConfigureRenderer(const WebRtc_UWord32 streamId,
+                                          const unsigned int zOrder,
+                                          const float left, const float top,
+                                          const float right,
+                                          const float bottom);
+
+  virtual WebRtc_Word32 SetText(const WebRtc_UWord8 textId,
+                                const WebRtc_UWord8* text,
+                                const WebRtc_Word32 textLength,
+                                const WebRtc_UWord32 textColorRef,
+                                const WebRtc_UWord32 backgroundColorRef,
+                                const float left, const float top,
+                                const float rigth, const float bottom);
+
+  virtual WebRtc_Word32 SetBitmap(const void* bitMap,
+                                  const WebRtc_UWord8 pictureId,
+                                  const void* colorKey, const float left,
+                                  const float top, const float right,
+                                  const float bottom);
+
+ protected:
+  virtual AndroidStream* CreateAndroidRenderChannel(
+      WebRtc_Word32 streamId,
+      WebRtc_Word32 zOrder,
+      const float left,
+      const float top,
+      const float right,
+      const float bottom,
+      VideoRenderAndroid& renderer) = 0;
+
+  WebRtc_Word32 _id;
+  CriticalSectionWrapper& _critSect;
+  VideoRenderType _renderType;
+  jobject _ptrWindow;
+
+  static JavaVM* g_jvm;
+
+ private:
+  static bool JavaRenderThreadFun(void* obj);
+  bool JavaRenderThreadProcess();
+
+  // Map with streams to render.
+  MapWrapper _streamsMap;
+  // True if the _javaRenderThread thread shall be detached from the JVM.
+  bool _javaShutDownFlag;
+  EventWrapper& _javaShutdownEvent;
+  EventWrapper& _javaRenderEvent;
+  WebRtc_Word64 _lastJavaRenderEvent;
+  JNIEnv* _javaRenderJniEnv; // JNIEnv for the java render thread.
+  ThreadWrapper* _javaRenderThread;
 };
 
 } //namespace webrtc
 
-
-#endif  // WEBRTC_MODULES_VIDEO_RENDER_MAIN_SOURCE_EXTERNAL_VIDEO_RENDER_EXTERNAL_IMPL_H_
+#endif  // WEBRTC_MODULES_VIDEO_RENDER_MAIN_SOURCE_BB_VIDEO_RENDER_BB_IMPL_H_
