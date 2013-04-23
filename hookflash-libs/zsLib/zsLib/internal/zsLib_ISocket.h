9127ec114dee5b485362bef15247a236c93c5709
Robin Raymond <robin@hookflash.com>
2013-04-23 18:32:54 -0400

    - fix SO_NOSIGPIPE to be unsupported on QNX/BB10

--- hookflash-libs/zsLib/zsLib/internal/zsLib_ISocket.h
+++ hookflash-libs/zsLib/zsLib/internal/zsLib_ISocket.h
@@ -55,13 +55,9 @@ namespace zsLib
     SO_EXCLUSIVEADDRUSE = -1,
     SO_DONTLINGER = -1,
     SO_MAX_MSG_SIZE = -1,
-#if (defined _ANDROID || defined __QNX__)
-    SO_NOSIGPIPE = MSG_NOSIGNAL,
-#else
-#ifdef _LINUX
+#if (defined _ANDROID || defined __QNX__ || defined _LINUX)
     SO_NOSIGPIPE = -1,
-#endif //_LINUX
-#endif //(defined _ANDROID || defined __QNX__)
+#endif //(defined _ANDROID || defined __QNX__ || defined _LINUX)
 
     INVALID_SOCKET = -1,
     SOCKET_ERROR = -1,
