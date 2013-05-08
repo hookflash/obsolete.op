f70fcf1b0cbfffaed71ebfcc6cc8d00911d914ec
Robin Raymond <robin@hookflash.com>
2013-05-08 16:31:51 -0400

    - added logging to when response error code is set manually

--- hookflash-core/hookflash/services/cpp/services_HTTP.cpp
+++ hookflash-core/hookflash/services/cpp/services_HTTP.cpp
@@ -925,7 +925,12 @@ namespace hookflash
         mResultCode = result;
         if (0 == mResponseCode) {
           if (CURLE_OK != result) {
-            mResponseCode = HTTPStatusCode_ClientClosedRequest;
+            if (mCurl) {
+              mResponseCode = HTTPStatusCode_MethodFailure;
+            } else {
+              mResponseCode = HTTPStatusCode_ClientClosedRequest;
+            }
+            ZS_LOG_DEBUG(log("manually result error") + ", error=" + toString(toStatusCode(mResponseCode)))
           }
         }
 
