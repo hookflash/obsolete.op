16ef3171040a1db9af4066a4270bb42deb44b210
Robin Raymond <robin@hookflash.com>
2013-08-05 11:44:32 -0400

    - removed "webRTC" library from hookflash-libs to ensure no paths are pointing to the old version (and they were)

--- hookflash-libs/webRTC/third_party/libturbojpeg-ios/build/libjpeg-turbo-ios.build/Debug-iphoneos/turbojpeg.build/Objects-normal/armv7/jdatasrc-tj.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/johnny/Documents/IDG/op/hookflash-libs/webRTC/third_party/libturbojpeg-ios/libturbojpeg/jdatasrc-tj.c \
-  /Users/johnny/Documents/IDG/op/hookflash-libs/webRTC/third_party/libturbojpeg-ios/libturbojpeg/jinclude.h \
-  /Users/johnny/Documents/IDG/op/hookflash-libs/webRTC/third_party/libturbojpeg-ios/libturbojpeg/jconfig.h \
-  /Users/johnny/Documents/IDG/op/hookflash-libs/webRTC/third_party/libturbojpeg-ios/libturbojpeg/jpeglib.h \
-  /Users/johnny/Documents/IDG/op/hookflash-libs/webRTC/third_party/libturbojpeg-ios/libturbojpeg/jmorecfg.h \
-  /Users/johnny/Documents/IDG/op/hookflash-libs/webRTC/third_party/libturbojpeg-ios/libjpeg/jerror.h
--- hookflash-libs/webRTC/third_party/libturbojpeg-ios/build/libjpeg-turbo-ios.build/Debug-iphoneos/turbojpeg.build/Script-427BF7451645DAF900BC3809.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mkdir -p "$(dirname "$SCRIPT_INPUT_FILE_0")/lib"
-cp "$SCRIPT_INPUT_FILE_0" "$SCRIPT_OUTPUT_FILE_0"
